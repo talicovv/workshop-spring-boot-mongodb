0d3ee98155f8bded644d30d6816e03f456704ad3
talicovv <soparacadastro80@gmail.com>
2018-10-21 14:53:49 -0300

    inserindo tratamento de erro

--- src/main/java/com/vvtalico/workshopmongo/services/UserService.java
+++ src/main/java/com/vvtalico/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vvtalico.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vvtalico.workshopmongo.domain.User;
 import com.vvtalico.workshopmongo.repository.UserRepository;
+import com.vvtalico.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,11 @@ public class UserService {
 		return repo.findAll();
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 }
