96bb2e55b9c2ca8e7b8e8450a72c0085f0c1bab6
talicovv <soparacadastro80@gmail.com>
2018-10-28 19:12:02 -0300

    obtendo post por id

---
+++ src/main/java/com/vvtalico/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.vvtalico.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vvtalico.workshopmongo.domain.Post;
+import com.vvtalico.workshopmongo.repository.PostRepository;
+import com.vvtalico.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}	
+}
