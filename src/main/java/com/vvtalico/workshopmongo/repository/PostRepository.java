8f91a9d0c8cf5765838e5601336b11c74d8a6d0c
talicovv <soparacadastro80@gmail.com>
2018-10-28 20:41:31 -0300

    fazendo consulta através do @query

--- src/main/java/com/vvtalico/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vvtalico/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.vvtalico.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vvtalico.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.vvtalico.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex:?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
