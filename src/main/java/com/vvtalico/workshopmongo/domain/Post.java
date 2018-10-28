f607f155b3e28eff3cdb49d59e5180d167e34e69
talicovv <soparacadastro80@gmail.com>
2018-10-28 19:41:46 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/vvtalico/workshopmongo/domain/Post.java
+++ src/main/java/com/vvtalico/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.vvtalico.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.vvtalico.workshopmongo.dto.AuthorDTO;
+import com.vvtalico.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post () {
 		
@@ -73,6 +78,14 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -98,6 +111,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
 
 }
