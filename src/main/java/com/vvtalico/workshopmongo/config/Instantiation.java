c8981e62dc0513df5f6503d6ec786ba3bf035b6e
talicovv <soparacadastro80@gmail.com>
2018-10-28 18:27:12 -0300

    criando AuthorDTO

--- src/main/java/com/vvtalico/workshopmongo/config/Instantiation.java
+++ src/main/java/com/vvtalico/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.vvtalico.workshopmongo.domain.Post;
 import com.vvtalico.workshopmongo.domain.User;
+import com.vvtalico.workshopmongo.dto.AuthorDTO;
 import com.vvtalico.workshopmongo.repository.PostRepository;
 import com.vvtalico.workshopmongo.repository.UserRepository;
 
@@ -36,12 +37,14 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para São Paulo",maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia ", "Acordei feliz hoje!",maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para São Paulo",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia ", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
 	}
 	
 	
